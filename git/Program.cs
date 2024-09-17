da5beeeaddcaf2a9580991a4771eb3d7118b5b85
alexanderek <Alexander.ek@live.se>
2024-09-17 10:25:03 +0200

    Added new Console.Writeline

--- git/Program.cs
+++ git/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace git;
+﻿using System.Xml;
+
+namespace git;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Här kommer en ny rad");
     }
 }
